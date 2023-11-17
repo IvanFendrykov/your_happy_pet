69543bca2828795fb5354eec93da609238535503
IvanFendrykov <118548628+IvanFendrykov@users.noreply.github.com>
2023-11-17 14:42:19 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './utils/theme.js';
-import App from './App.jsx';
+// import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
