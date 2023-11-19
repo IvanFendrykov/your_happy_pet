1c8b34ebb03baf84ec1a3b1085477ef4955ab5bf
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-19 21:04:09 +0200

    Create NoticesSearch.jsx

---
+++ src/components/NoticesSearch/NoticesSearch.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+const NoticesSearch = () => {
+  return (
+    <form>
+      <input type="text" />
+      <button type="submit"></button>
+    </form>
+  );
+};
