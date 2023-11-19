62a0a92c5cec84213aaa5da53e8a702bb6dca1f5
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-19 21:03:36 +0200

    add pet gallery components

---
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -0,0 +1,19 @@
+import { NoticesCategoriesItem } from './NoticesCategoriesItem';
+
+const NoticesCategoriesList = ({ items }) => {
+  return (
+    <div>
+      {items.map(item => (
+        <NoticesCategoriesItem
+          description={item.description}
+          category={item.category}
+          place={item.place}
+          age={item.age}
+          gender={item.gender}
+        />
+      ))}
+    </div>
+  );
+};
+
+export { NoticesCategoriesList };
---
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -0,0 +1,17 @@
+// sell
+// lost/found
+// in good hands
+// favorite ads
+// my ads
+
+const NoticesCategoriesNav = () => {
+  return (
+    <div>
+      <div>sell</div>
+      <div>lost/found</div>
+      <div>in good hands</div>
+      <div>favorite ads</div>
+      <div>my ads</div>
+    </div>
+  );
+};
