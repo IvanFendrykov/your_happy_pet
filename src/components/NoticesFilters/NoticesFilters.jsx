7bdcc07d4336e20583fdaa7ad3104f3d9e49353a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-19 22:12:50 +0200

    Update NoticesFilters.jsx

--- src/components/NoticesFilters/NoticesFilters.jsx
+++ src/components/NoticesFilters/NoticesFilters.jsx
@@ -12,16 +12,31 @@ const NoticesFilters = () => {
         (<button>By age</button>)(
           isByAgeOpen && (
             <form>
-              <label>up to 1 year</label>
-              <label>up to 2 years</label>
-              <label>from 2 years</label>
+              <label>
+                <input type="checkbox" />
+                up to 1 year
+              </label>
+              <label>
+                <input type="checkbox" />
+                up to 2 years
+              </label>
+              <label>
+                <input type="checkbox" />
+                from 2 years
+              </label>
             </form>
           ),
         )(<button>By gender</button>)(
           isByGenderOpen && (
             <form>
-              <label>female</label>
-              <label>male</label>
+              <label>
+                <input type="checkbox" />
+                female
+              </label>
+              <label>
+                <input type="checkbox" />
+                male
+              </label>
             </form>
           ),
         )}
