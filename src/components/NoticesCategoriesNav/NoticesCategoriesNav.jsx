96b0fd534357000f7f1d8a505019e7897c4e6c28
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-19 22:43:32 +0200

    Update NoticesCategoriesNav.jsx

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -4,23 +4,35 @@ const NoticesCategoriesNav = () => {
   return (
     <form>
       <label>
-        <input type="radio" name="noticesCategoriesNavItem" />
+        <input type="radio" name="noticesCategoriesNavItem" value="sell" />
         sell
       </label>
       <label>
-        <input type="radio" name="noticesCategoriesNavItem" />
+        <input
+          type="radio"
+          name="noticesCategoriesNavItem"
+          value="lost/found"
+        />
         lost/found
       </label>
       <label>
-        <input type="radio" name="noticesCategoriesNavItem" />
+        <input
+          type="radio"
+          name="noticesCategoriesNavItem"
+          value="in good hands"
+        />
         in good hands
       </label>
       <label>
-        <input type="radio" name="noticesCategoriesNavItem" />
+        <input
+          type="radio"
+          name="noticesCategoriesNavItem"
+          value="favorite ads"
+        />
         favorite ads
       </label>
       <label>
-        <input type="radio" name="noticesCategoriesNavItem" />
+        <input type="radio" name="noticesCategoriesNavItem" value="my ads" />
         my ads
       </label>
     </form>
