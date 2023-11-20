7de39168c1b9f47e74b92ba21a321cebc5fa8e92
IvanFendrykov <118548628+IvanFendrykov@users.noreply.github.com>
2023-11-20 16:50:20 +0100

    Revert "Revert "fix critical problem and add privat route""
    This reverts commit b8057a25bb941ffcac025fa83acc6f2a361fbb7e.

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -2,55 +2,54 @@ import { useEffect, useState } from 'react';
 import { fetchOurFriendsApi } from '../../servises/fetchOurFriendApi';
 
 const OurFriendsPage = () => {
+  const [ourFriends, setOurFriends] = useState([]);
 
-    const [ourFriends, setOurFriends] = useState([]);
+  useEffect(() => {
+    const fetchOurFriends = async () => {
+      try {
+        const response = await fetchOurFriendsApi();
+        setOurFriends(response);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-    useEffect(() => {
-        const fetchOurFriends = async () => {
-            try {
-                const response = await fetchOurFriendsApi();
-                setOurFriends(response);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
+    fetchOurFriends();
+  }, []);
 
-        fetchOurFriends();
-    }, []);
-
-    return (
-        <div>
-            <h1>Our friends</h1>
-            <ul>
-                {ourFriends.map(ourFriend => (
-                    <li key={ourFriend.title}>
-                        <h2>{ourFriend.title}</h2>
-                        <div>
-                            <img src={ourFriend.img} alt={ourFriend.title} />
-                            <ul>
-                                <li>
-                                    <h3>Time:</h3>
-                                    <span>{ourFriend.time}</span>
-                                </li>
-                                <li>
-                                    <h3>Adress:</h3>
-                                    <span>{ourFriend.adress}</span>
-                                </li>
-                                <li>
-                                    <h3>Email:</h3>
-                                    <span>{ourFriend.email}</span>
-                                </li>
-                                <li>
-                                    <h3>Phone:</h3>
-                                    <span>{ourFriend.phone}</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Our friends</h1>
+      <ul>
+        {ourFriends.map((ourFriend) => (
+          <li key={ourFriend.title}>
+            <h2>{ourFriend.title}</h2>
+            <div>
+              <img src={ourFriend.img} alt={ourFriend.title} />
+              <ul>
+                <li>
+                  <h3>Time:</h3>
+                  <span>{ourFriend.time}</span>
+                </li>
+                <li>
+                  <h3>Adress:</h3>
+                  <span>{ourFriend.adress}</span>
+                </li>
+                <li>
+                  <h3>Email:</h3>
+                  <span>{ourFriend.email}</span>
+                </li>
+                <li>
+                  <h3>Phone:</h3>
+                  <span>{ourFriend.phone}</span>
+                </li>
+              </ul>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default OurFriendsPage;
