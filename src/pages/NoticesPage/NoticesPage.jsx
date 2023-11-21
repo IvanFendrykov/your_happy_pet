e3786775ec9fe63282a087ce4e3c9ed53c95b0a5
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-21 22:38:42 +0200

    Update NoticesPage.jsx

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -8,18 +8,18 @@ import { NoticesFilters } from '../../components/NoticesFilters/NoticesFilters';
 import { NoticesCategoriesList } from '../../components/NoticesCategoriesList/NoticesCategoriesList';
 
 const pets = [
-  {category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
-  {category:"lost/found", place: "Lviv", age: "2 years", gender: "female", description: "Сute dog looking for a home"},
-  {category:"favorite ads", place: "Khmelnytskyi", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
-  {category:"my ads", place: "Lviv", age: "2 years", gender: "female", description: "Сute dog looking for a home"},
-  {category:"sell", place: "Ivano - Frankivsk", age: "1 year", gender: "male", description: "Сute dog looking for a home"},
-  {category:"in good hands", place: "Ivano - Frankivsk", age: "3 years", gender: "female", description: "Сute dog looking for a home"},
-  {category:"lost/found", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
-  {category:"my ads", place: "Ivano - Frankivsk", age: "2 years", gender: "male", description: "Сute dog looking for a home"},
-  {category:"in good hands", place: "Chernivtsi", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
-  {category:"favorite ads", place: "Ivano - Frankivsk", age: "5 years", gender: "male", description: "Сute dog looking for a home"},
-  {category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
-  {category:"lost/found", place: "Zaporizhzhia", age: "4 years", gender: "male", description: "Сute dog looking for a home"}
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"lost/found", place: "Lviv", age: "2 years", gender: "female", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"favorite ads", place: "Khmelnytskyi", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"my ads", place: "Lviv", age: "2 years", gender: "female", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"sell", place: "Ivano - Frankivsk", age: "1 year", gender: "male", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"in good hands", place: "Ivano - Frankivsk", age: "3 years", gender: "female", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"lost/found", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"my ads", place: "Ivano - Frankivsk", age: "2 years", gender: "male", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"in good hands", place: "Chernivtsi", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"favorite ads", place: "Ivano - Frankivsk", age: "5 years", gender: "male", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
+  {imageLink: "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U", category:"lost/found", place: "Zaporizhzhia", age: "4 years", gender: "male", description: "Сute dog looking for a home"}
 ];
 
 const NoticesPage = () => {
