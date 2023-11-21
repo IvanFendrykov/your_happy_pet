3030408858f7961dde674d0321be36a676c9567f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-21 22:22:48 +0200

    Update NoticesPage.jsx

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -15,9 +15,9 @@ const pets = [
   {category:"sell", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
   {category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
   {category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
+  {category:"in good hands", place: "Ivano - Frankivsk", age: "2 years", gender: "female", description: "Сute dog looking for a home"},
   {category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
-  {category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
-  {category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
+  {category:"in good hands", place: "Ivano - Frankivsk", age: "5 years", gender: "female", description: "Сute dog looking for a home"},
   {category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"},
   {category:"in good hands", place: "Ivano - Frankivsk", age: "1 year", gender: "female", description: "Сute dog looking for a home"}
 ];
