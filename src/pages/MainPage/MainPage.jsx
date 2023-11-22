f132e458a978237ad61724d59178cbceac6df353
Arina Miahkykh <79450238+Arina-M@users.noreply.github.com>
2023-11-22 18:30:45 +0200

    fixed styles

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,17 @@
+import Header from '../../components/Header/Header.jsx';
+import './MainPage.css';
+//import Animals from '../../images/main-page/right-animals.png'
+
+const MainPage = () => {
+    return (
+        <>
+            <Header></Header>
+            <div className='main-page'>
+                <h1 >Take good care <br className='br-desktop' />of<br className='br-tablet' /> your small<br className='br-desktop' /> pets</h1>
+                {/* <img src={Animals} alt='Animals'></img> */}
+            </div>
+        </>
+    );
+};
+
+export default MainPage;
