bb292b0fbede19de19e575becb8ce584491dbb4f
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2023-11-24 00:51:37 +0200

    feat: added userpage to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-  import { Suspense } from 'react';
-  import { lazy } from 'react';
-  import { Route, Routes } from 'react-router-dom';
-  import {Toaster} from 'react-hot-toast';
+import { Suspense } from 'react';
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import VortexLoader from './components/VortexLoader/VortexLoader';
 // import PublicRoute from './PublicRoute/PublicRoute';
 // import PrivateRoute from './PrivateRoute/PrivateRoute';
 // import Loader from './components/Loader/Loader';
 
- import Layout from './components/Layout/Layout';
+import Layout from './components/Layout/Layout';
 // import {NoticesCategoriesList} from './components/NoticesCategoriesList/NoticesCategoriesList';
 // import {AddPetPage} from './pages/AddPetPage/AddPetPage';
 
- const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
- const RegisterPage = lazy(() => import('./components/Register/Register.jsx'));
- const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
- const UserPage = lazy(() => import('./pages/UserPage/UserPage'));
- const NewsPage = lazy(() => import('./pages/NewsPage/NewsPage'));
+const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
+const RegisterPage = lazy(() => import('./components/Register/Register.jsx'));
+const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+const UserPage = lazy(() => import('./pages/UserPage/UserPage'));
+const NewsPage = lazy(() => import('./pages/NewsPage/NewsPage'));
 // const NoticesPage = lazy(() => import('./pages/NoticesPage/NoticesPage'));
 // const OurFriendsPage = lazy(() =>
 //   import('./pages/OurFriendsPage/OurFriendsPage')
@@ -36,12 +36,13 @@ import VortexLoader from './components/VortexLoader/VortexLoader';
 export const App = () => {
   return (
     <>
-        <Suspense fallback={<VortexLoader />}>
+      <Suspense fallback={<VortexLoader />}>
         <Routes>
-           <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Layout />}>
             <Route index element={<MainPage />} />
             <Route path="main" element={<MainPage />} />
-            <Route path="news" element={<NewsPage />} /> 
+            <Route path="news" element={<NewsPage />} />
+            <Route path="user" element={<UserPage />} />
             {/* <Route path="notices" element={<NoticesPage />}>
               <Route index element={<NoticesCategoriesList />} />
               <Route path="sell" element={<NoticesCategoriesList />} />
@@ -50,25 +51,25 @@ export const App = () => {
               <Route path="favorite" element={<NoticesCategoriesList />} />
               <Route path="own" element={<NoticesCategoriesList />} />
             </Route> */}
-             {/* <Route path="friends" element={<OurFriendsPage />} />
+            {/* <Route path="friends" element={<OurFriendsPage />} />
             <Route element={<PublicRoute />}>
                 <Route path="register" element={<RegisterPage />} />
               <Route path="login" element={<LoginPage />} />
             </Route>
             <Route element={<PrivateRoute />}>
 
-              <Route path="user" element={<UserPage />} />
+              
               {/* <Route path="add-pet" element={<AddPetPage />} /> */}
             {/* </Route>
-            <Route path="*" element={<NotFoundPage />} /> */} 
+            <Route path="*" element={<NotFoundPage />} /> */}
 
-             </Route>
+          </Route>
         </Routes>
       </Suspense>
-       <Toaster
-          toastOptions={{
+      <Toaster
+        toastOptions={{
           duration: 3000,
-          }}
+        }}
       />
     </>
   );
