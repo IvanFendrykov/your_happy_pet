fb1d5647c440dcb4c1f3e7856b86e9b4cdbace27
vilhota30 <111867115+vilhota30@users.noreply.github.com>
2023-11-23 19:49:07 +0200

    adds Loader

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -6,6 +6,7 @@ const initialState = {
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
+  isloading: false,
 };
 
 const authSlice = createSlice({
@@ -16,6 +17,13 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
+    },
+    [register.pending](state, action){
+      state.isloading = true;
+    },
+    [register.rejected](state, action){
+      state.isloading = false;
+      state.error = action.payload;
     },
     [login.fulfilled](state, action) {
       state.user = action.payload.user;
