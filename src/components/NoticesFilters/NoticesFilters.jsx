4e2a6f05e73796e5836117e178d020b06e815602
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-23 20:41:48 +0200

    update notices page files

--- src/components/NoticesFilters/NoticesFilters.jsx
+++ src/components/NoticesFilters/NoticesFilters.jsx
@@ -1,45 +1,68 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { AgeForm } from './AgeForm';
+import { GenderForm } from './GenderForm';
 
-const NoticesFilters = () => {
+const NoticesFilters = ({ onChange }) => {
   const [isFiltersOpen, setIsFiltersOpen] = useState(false);
   const [isByAgeOpen, setIsByAgeOpen] = useState(false);
   const [isByGenderOpen, setIsByGenderOpen] = useState(false);
+  const [age, setAge] = useState({
+    upToOne: false,
+    upToTwo: false,
+    fromTwo: false,
+  });
+  const [gender, setGender] = useState({
+    female: false,
+    male: false,
+  });
+
+  const toggleFilters = () => {
+    if (isFiltersOpen) {
+      setIsFiltersOpen(false);
+    } else {
+      setIsFiltersOpen(true);
+    }
+  };
+
+  const toggleAgeFilter = () => {
+    if (isByAgeOpen) {
+      setIsByAgeOpen(false);
+    } else {
+      setIsByAgeOpen(true);
+    }
+  };
+
+  const toggleGenderFilter = () => {
+    if (isByGenderOpen) {
+      setIsByGenderOpen(false);
+    } else {
+      setIsByGenderOpen(true);
+    }
+  };
+
+  const selectAge = ageData => {
+    setAge(ageData);
+  };
+
+  const selectGender = genderData => {
+    setGender(genderData);
+  };
+
+  useEffect(() => {
+    onChange({ age, gender });
+  }, [age, gender, onChange]);
 
   return (
     <div>
-      <button>Filters</button>
-      {isFiltersOpen &&
-        (<button>By age</button>)(
-          isByAgeOpen && (
-            <form>
-              <label>
-                <input type="checkbox" />
-                up to 1 year
-              </label>
-              <label>
-                <input type="checkbox" />
-                up to 2 years
-              </label>
-              <label>
-                <input type="checkbox" />
-                from 2 years
-              </label>
-            </form>
-          ),
-        )(<button>By gender</button>)(
-          isByGenderOpen && (
-            <form>
-              <label>
-                <input type="checkbox" />
-                female
-              </label>
-              <label>
-                <input type="checkbox" />
-                male
-              </label>
-            </form>
-          ),
-        )}
+      <button onClick={toggleFilters}>Filters</button>
+      {isFiltersOpen && (
+        <div>
+          <button onClick={toggleAgeFilter}>By age</button>
+          {isByAgeOpen && <AgeForm onChange={selectAge} />}
+          <button onClick={toggleGenderFilter}>By gender</button>
+          {isByGenderOpen && <GenderForm onChange={selectGender} />}
+        </div>
+      )}
     </div>
   );
 };
