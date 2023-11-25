c7ff569b68dced73843bbc6d718735ad76dab63e
VladRaFeed <122728801+VladRaFeed@users.noreply.github.com>
2023-11-26 00:14:38 +0200

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-// import { Suspense } from 'react';
+import { Suspense } from 'react';
 import { lazy } from 'react';
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-// import PublicRoute from './PublicRoute/PublicRoute';
-// import PrivateRoute from './PrivateRoute/PrivateRoute';
+import PublicRoute from './PublicRoute/PublicRoute';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
 
-// import Loader from './components/Loader/Loader';
+import Loader from './components/Loader/Loader';
 
 // const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
 // const RegisterPage = lazy(() => import('./pages/RegisterPage/RegisterPage'));
 // const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
 // const UserPage = lazy(() => import('./pages/UserPage/UserPage'));
-const NewsPage = lazy(() => import('./pages/NewsPage/NewsPage'));
+// const NewsPage = lazy(() => import('./pages/NewsPage/NewsPage'));
 // const NoticesPage = lazy(() => import('./pages/NoticesPage/NoticesPage'));
 // const OurFriendsPage = lazy(() =>
 //   import('./pages/OurFriendsPage/OurFriendsPage')
 // );
-// const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 export const App = () => {
   return (
     <>
-      
-      <NewsPage/>
-
-      {/* <Suspense fallback={<Loader />}>
+      <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
+          {/* <Route path="/" element={<Layout />}> */}
+            {/* <Route index element={<MainPage />} />
             <Route path="main" element={<MainPage />} />
             <Route path="news" element={<NewsPage />} />
             <Route path="notices" element={<NoticesPage />}>
@@ -46,11 +43,11 @@ export const App = () => {
             <Route element={<PrivateRoute />}>
               <Route path="user" element={<UserPage />} />
               <Route path="add-pet" element={<AddPetPage />} />
-            </Route>
+            </Route> */}
             <Route path="*" element={<NotFoundPage />} />
           </Route>
         </Routes>
-      </Suspense> */}
+      </Suspense>
     </>
   );
 };
