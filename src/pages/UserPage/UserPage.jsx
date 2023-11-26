198c3bb4f646e1323219b32ea1f04295d0767cd1
Roman Telik <rtelik58@gmail.com>
2023-11-26 15:46:49 +0200

    figured-svg

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,118 +1,174 @@
-import { useState } from 'react'
-import PetsList from '../../components/PetsList/PetsList'
-import { SaveButton, EditProfileButton, H2, Label, ListItem, LogoutButton, Section, Span, StyledList, UserCardWrapper, UserProfileImage, Wrapper } from './UserPage.styled'
+import { useState } from 'react';
+import PetsList from '../../components/PetsList/PetsList';
+import {
+  SaveButton,
+  EditProfileButton,
+  H2,
+  Label,
+  ListItem,
+  LogoutButton,
+  Section,
+  Span,
+  StyledList,
+  UserCardWrapper,
+  UserProfileImage,
+  Wrapper,
+} from './UserPage.styled';
+import { ReactComponent as Edit } from '../../images/svg/edit.svg';
+import { ReactComponent as CrossSmall } from '../../images/svg/cross-small.svg';
 
 const user = {
-    name: "fakeName",
-    email: 'fakeEmail',
-    bday: '11.11',
-    phone: '111-111-111',
-    city: 'Kyiv'
-}
+  name: 'fakeName',
+  email: 'fakeEmail',
+  bday: '11.11',
+  phone: '111-111-111',
+  city: 'Kyiv',
+};
 
-const pets = [{
+const pets = [
+  {
     id: 2,
     imageUrl: 'https://placekitten.com/200/200',
     name: 'Fluffy',
     dateOfBirth: '2020-01-01',
     type: 'Cat',
     comments: 'A cute and fluffy kitty.',
-}, {
+  },
+  {
     id: 1,
     imageUrl: 'https://placekitten.com/200/210',
     name: 'Fluffy',
     dateOfBirth: '2020-01-01',
     type: 'Cat',
     comments: 'A cute and fluffy kitty.',
-}]
-
-
-
-
+  },
+];
 
 function UserPage() {
-    const [clicked, setClicked] = useState(false) 
-    const [currentUser, setCurrentUser] = useState(user)//it is temporary, need to send this obj on back
-    const [currentName, setCurrentName] = useState(currentUser.name)
-    const [currentEmail, setCurrentEmail] = useState(currentUser.email)
-    const [currentBday, setCurrentBday] = useState(currentUser.bday)
-    const [currentPhone, setCurrentPhone] = useState(currentUser.phone)
-    const [currentCity, setCurrentCity] = useState(currentUser.city)
+  const [clicked, setClicked] = useState(false);
+  const [currentUser, setCurrentUser] = useState(user); //it is temporary, need to send this obj on back
+  const [currentName, setCurrentName] = useState(currentUser.name);
+  const [currentEmail, setCurrentEmail] = useState(currentUser.email);
+  const [currentBday, setCurrentBday] = useState(currentUser.bday);
+  const [currentPhone, setCurrentPhone] = useState(currentUser.phone);
+  const [currentCity, setCurrentCity] = useState(currentUser.city);
 
-    const toggleForm = (e) => {
-        e.preventDefault()
-        setClicked(!clicked)
-        console.log(clicked);
-    }
-      const onUserNameChange = (e) => {
-        setCurrentName(e.target.value)
-    }
-    const onUserEmailChange = (e) => {
-        setCurrentEmail(e.target.value)
-    }
-    const onUserBdayChange = (e) => {
-        setCurrentBday(e.target.value)
-    }
-    const onUserPhoneChange = (e) => {
-        setCurrentPhone(e.target.value)
-    }
-    const onUserCityChange = (e) => {
-        setCurrentCity(e.target.value)
-    }
-    const submitForm = (e) => {
-        e.preventDefault()
-        const editedUser = {
-            name: currentName,
-            email: currentEmail,
-            bday: currentBday,
-            phone: currentPhone,
-            city: currentCity
-        }
-      
-        console.log(editedUser);
-        setCurrentUser(editedUser)
-        setClicked(false)
-    }
-    return (
-        <>
-            <Wrapper>
-                <Section>
-                    <H2>My information:</H2>
-                    <UserCardWrapper>
-                        <UserProfileImage />
-                        <StyledList>
-                            <ListItem>
-                                <Label>Name:</Label>
-                                <Span type="text" name="name" value={currentName} disabled={!clicked} onChange={onUserNameChange}></Span>
-                            </ListItem>
-                            <ListItem>
-                                <Label>Email:</Label>
-                                <Span type="email" name="email" value={currentEmail} disabled={!clicked} onChange={onUserEmailChange}></Span>
-                            </ListItem>
-                            <ListItem>
-                                <Label>Birthday:</Label>
-                                <Span type="text" name="birthday" value={currentBday} disabled={!clicked} onChange={onUserBdayChange}></Span>
-                            </ListItem>
-                            <ListItem>
-                                <Label>Phone:</Label>
-                                <Span type="text" name="phone" value={currentPhone} disabled={!clicked} onChange={onUserPhoneChange}></Span>
-                            </ListItem>
-                            <ListItem>
-                                <Label>City:</Label>
-                                <Span type="text" name="city" value={currentCity} disabled={!clicked} onChange={onUserCityChange}></Span>
-                            </ListItem>
-                        </StyledList>
-                        {clicked ? <SaveButton onClick={submitForm}>save</SaveButton> : <LogoutButton>logout</LogoutButton>}
-                        <EditProfileButton onClick={toggleForm}>Edit</EditProfileButton>
-                    </UserCardWrapper>
-                </Section>
-                <Section>
-                    <H2>My pets:</H2>
-                    <PetsList pets={pets} />
-                </Section>
-            </Wrapper>
-        </>
-    )
+  const toggleForm = (e) => {
+    e.preventDefault();
+    setClicked(!clicked);
+    console.log(clicked);
+  };
+  const onUserNameChange = (e) => {
+    setCurrentName(e.target.value);
+  };
+  const onUserEmailChange = (e) => {
+    setCurrentEmail(e.target.value);
+  };
+  const onUserBdayChange = (e) => {
+    setCurrentBday(e.target.value);
+  };
+  const onUserPhoneChange = (e) => {
+    setCurrentPhone(e.target.value);
+  };
+  const onUserCityChange = (e) => {
+    setCurrentCity(e.target.value);
+  };
+  const submitForm = (e) => {
+    e.preventDefault();
+    const editedUser = {
+      name: currentName,
+      email: currentEmail,
+      bday: currentBday,
+      phone: currentPhone,
+      city: currentCity,
+    };
+
+    console.log(editedUser);
+    setCurrentUser(editedUser);
+    setClicked(false);
+  };
+  return (
+    <>
+      <Wrapper>
+        <Section>
+          <H2>My information:</H2>
+          <UserCardWrapper>
+            <UserProfileImage />
+            <StyledList>
+              <ListItem>
+                <Label>Name:</Label>
+                <Span
+                  type="text"
+                  name="name"
+                  value={currentName}
+                  disabled={!clicked}
+                  onChange={onUserNameChange}
+                ></Span>
+              </ListItem>
+              <ListItem>
+                <Label>Email:</Label>
+                <Span
+                  type="email"
+                  name="email"
+                  value={currentEmail}
+                  disabled={!clicked}
+                  onChange={onUserEmailChange}
+                ></Span>
+              </ListItem>
+              <ListItem>
+                <Label>Birthday:</Label>
+                <Span
+                  type="text"
+                  name="birthday"
+                  value={currentBday}
+                  disabled={!clicked}
+                  onChange={onUserBdayChange}
+                ></Span>
+              </ListItem>
+              <ListItem>
+                <Label>Phone:</Label>
+                <Span
+                  type="text"
+                  name="phone"
+                  value={currentPhone}
+                  disabled={!clicked}
+                  onChange={onUserPhoneChange}
+                ></Span>
+              </ListItem>
+              <ListItem>
+                <Label>City:</Label>
+                <Span
+                  type="text"
+                  name="city"
+                  value={currentCity}
+                  disabled={!clicked}
+                  onChange={onUserCityChange}
+                ></Span>
+              </ListItem>
+            </StyledList>
+            {clicked ? (
+              <SaveButton onClick={submitForm}>save</SaveButton>
+            ) : (
+              <LogoutButton>logout</LogoutButton>
+            )}
+            {clicked ? (
+              <EditProfileButton onClick={toggleForm}>
+                <CrossSmall style={{ stroke: '#54ADFF' }}></CrossSmall>
+              </EditProfileButton>
+            ) : (
+              <EditProfileButton onClick={toggleForm}>
+                <Edit style={{ stroke: '#54ADFF' }}></Edit>
+              </EditProfileButton>
+            )}
+          </UserCardWrapper>
+        </Section>
+        <Section>
+          <H2>My pets:</H2>
+          <PetsList pets={pets} />
+        </Section>
+      </Wrapper>
+    </>
+  );
 }
 
-export default UserPage
+export default UserPage;
--- src/pages/UserPage/UserPage.styled.jsx
+++ src/pages/UserPage/UserPage.styled.jsx
@@ -1,25 +1,27 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-    margin-left: auto;
-    margin-right: auto;
-    width: 320px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 320px;
+  padding-top: 40px;
 
-    @media (min-width: 768px) {
-      width: 768px;
-    }
+  @media (min-width: 768px) {
+    width: 768px;
+    padding-top: 60px;
+  }
 
-    @media (min-width: 1280px) {
-      display: flex;
-      width: 1280px;
-    }
-`
+  @media (min-width: 1280px) {
+    display: flex;
+    width: 1280px;
+  }
+`;
 
 export const Section = styled.section`
-padding-right: 20px;
-padding-left: 20px;
-margin-bottom: 46px;
-`
+  padding-right: 20px;
+  padding-left: 20px;
+  margin-bottom: 46px;
+`;
 
 export const H2 = styled.h2`
   font-size: 20px;
@@ -29,37 +31,37 @@ export const H2 = styled.h2`
 `;
 
 export const UserCardWrapper = styled.form`
-position: relative;
-border-radius: 20px;
-background: #FFF;
-box-shadow: 3px 8px 14px 0px rgba(136, 198, 253, 0.19);
-padding: 20px 8px 25px 8px;
-width: 280px;
-flex-shrink: 0;
+  position: relative;
+  border-radius: 20px;
+  background: #fff;
+  box-shadow: 3px 8px 14px 0px rgba(136, 198, 253, 0.19);
+  padding: 20px 8px 25px 8px;
+  width: 280px;
+  flex-shrink: 0;
   @media (min-width: 768px) {
     display: flex;
     flex-direction: row-reverse;
     justify-content: space-between;
     width: 704px;
     padding-right: 76px;
-    padding-bottom: 58px
+    padding-bottom: 58px;
   }
 
-    @media (min-width: 1280px) {
-      display: block;
-      width: 395px;
-    }
-
-`
+  @media (min-width: 1280px) {
+    display: block;
+    width: 395px;
+    padding-right: 0;
+  }
+`;
 
 export const UserProfileImage = styled.div`
-width: 182px;
-height: 182px;
-flex-shrink: 0;
-background-color: grey;
-margin-left: auto;
-margin-right: auto;
-margin-bottom: 60px;
+  width: 182px;
+  height: 182px;
+  flex-shrink: 0;
+  background-color: grey;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 60px;
   @media (min-width: 768px) {
     margin-left: 0;
     margin-right: 0;
@@ -68,66 +70,71 @@ margin-bottom: 60px;
     margin-left: auto;
     margin-right: auto;
   }
-`
+`;
 
 export const StyledList = styled.ul`
-list-style-type: none;
-padding: 0;
+  list-style-type: none;
+  padding: 0;
   @media (min-width: 768px) {
-width: 320px
+    width: 320px;
   }
-`
+  @media (min-width: 1280px) {
+    width: 356px;
+  }
+`;
 
 export const ListItem = styled.li`
   display: flex;
   gap: 4px;
   margin-bottom: 20px;
   &:last-child {
-  margin-bottom: 0; 
+    margin-bottom: 0;
   }
-`
+`;
 
 export const Label = styled.span`
-font-size: 14px;
-font-weight: 600;
-letter-spacing: 0.56px;
-width: 100px;
-padding: 4px;
-`
+  font-size: 14px;
+  font-weight: 600;
+  letter-spacing: 0.56px;
+  width: 100px;
+  padding: 4px;
+`;
 
 export const Span = styled.input`
-display: flex;
-font-size: 12px;
-letter-spacing: 0.48px;
-width: 100%;
-border-radius: 20px;
-border: 1px solid var(--unnamed, #54ADFF);
-padding: 4px 10px;
-`
+  display: flex;
+  font-size: 12px;
+  letter-spacing: 0.48px;
+  width: 100%;
+  border-radius: 20px;
+  border: 1px solid var(--unnamed, #54adff);
+  padding: 4px 10px;
+`;
 
 export const LogoutButton = styled.button`
-position: absolute;
-left: 10px;
-bottom: 14px;
+  position: absolute;
+  left: 10px;
+  bottom: 14px;
   @media (min-width: 768px) {
     left: 20px;
   }
-`
+`;
 
 export const EditProfileButton = styled.button`
-position: absolute;
-right: 10px;
-top: 14px;
+  position: absolute;
+  right: 10px;
+  top: 14px;
+  background-color: transparent;
+  border: transparent;
   @media (min-width: 768px) {
     right: 20px;
   }
-`
+`;
 
 export const SaveButton = styled.button`
-position: absolute;
-left: 10px;
-bottom: 14px;
+  position: absolute;
+  left: 10px;
+  bottom: 14px;
   @media (min-width: 768px) {
     left: 20px;
   }
-`
+`;
