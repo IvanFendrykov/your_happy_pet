07a2ee44fc400f4f26f733c5f51850d7ab3bcb48
IvanFendrykov <118548628+IvanFendrykov@users.noreply.github.com>
2023-11-26 19:41:21 +0100

    fix privat route Loader

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -2,14 +2,14 @@ import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 
 import { selectAuth } from '../redux/auth/auth-selectors';
-import VortexLoader from '../components/VortexLoader/VortexLoader';
+// import VortexLoader from '../components/VortexLoader/VortexLoader';
 
 const PrivateRoute = () => {
   const { isLoggedIn, token } = useSelector(selectAuth);
 
-  if (!isLoggedIn && token) {
-    return <VortexLoader />;
-  }
+  // if (!isLoggedIn && token) {
+  //   return <VortexLoader />;
+  // }
 
   if (!isLoggedIn && !token) {
     return <Navigate to="/login" />;
