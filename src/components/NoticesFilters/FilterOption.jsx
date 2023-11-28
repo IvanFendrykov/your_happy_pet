7170a4438cd23ee14f6a7dc4767172a93505dd3c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-28 02:08:34 +0200

    Create FilterOption.jsx

---
+++ src/components/NoticesFilters/FilterOption.jsx
@@ -0,0 +1,26 @@
+import {
+  HiddenRadio,
+  CheckedOption,
+  UnCheckedOption,
+} from './NoticesCategoriesNav.styled';
+
+const FilterOption = ({ name, value, checked, defaultChecked, children }) => {
+  return checked ? (
+    <CheckedOption>
+      <HiddenRadio
+        type="radio"
+        name={name}
+        value={value}
+        defaultChecked={defaultChecked}
+      />
+      {children}
+    </CheckedOption>
+  ) : (
+    <UnCheckedOption>
+      <HiddenRadio type="radio" name={name} value={value} />
+      {children}
+    </UnCheckedOption>
+  );
+};
+
+export { FilterOption };
