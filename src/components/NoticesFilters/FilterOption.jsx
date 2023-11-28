67b897fc15ff0cdbd3d96651e5daf9d185d052c8
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-28 03:16:14 +0200

    Update FilterOption.jsx

--- src/components/NoticesFilters/FilterOption.jsx
+++ src/components/NoticesFilters/FilterOption.jsx
@@ -4,25 +4,14 @@ import { HiddenRadio, FilterOption } from './NoticesFilters.styled';
 const FilterOption = ({ name, value, checked, defaultChecked, children }) => {
   return (
     <FilterOption>
-      <HiddenRadio
-        type="radio"
-        name={name}
-        value={value}
-        defaultChecked={defaultChecked}
-      />
+      <HiddenRadio type="radio" name={name} value={value} />
 
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-      >
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
         <use xlinkHref={`${icons}#${checked ? 'check-round' : 'round'}`} />
       </svg>
       {children}
     </FilterOption>
   );
 };
-//      checked ?
+//        defaultChecked={defaultChecked}
 export { FilterOption };
