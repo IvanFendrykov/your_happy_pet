0b7eb29262e37de962c3892410de3d5fc9fe76d9
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-30 01:02:48 +0200

    Update NoticesPage.styled.js

--- src/pages/NoticesPage/NoticesPage.styled.js
+++ src/pages/NoticesPage/NoticesPage.styled.js
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { theme } from '../../utils/theme';
-const { fonts, fontWeights } = theme;
+const { fonts, fontSizes, fontWeights, colors } = theme;
 
 const Header = styled.h3`
   margin-top: 90px;
@@ -25,31 +25,27 @@ const NoticePageContrtolsRight = styled.div`
 
 const FilterOpenButton = styled.button`
   display: flex;
-  width: 152px;
-  padding: 4px 10px;
+  width: 129px;
+  padding: 8px 20px;
   justify-content: center;
   align-items: center;
   gap: 8px;
-  border-radius: 40px;
-  border: 2px solid ${colors.blue};
-  background-color: #fff;
 
-  color: #54adff;
+  border-radius: 40px;
+  background: ${colors.blue};
+
+  color: #fef9f9;
   font-family: ${fonts.bold}, sans-serif;
-  font-size: 16px;
+  font-size: ${fontSizes.m};
   font-style: normal;
   font-weight: ${fontWeights.bold};
   line-height: normal;
   letter-spacing: 0.64px;
-
-  :hover,
-  :focus {
-    padding: 6px 12px;
-    border: none;
-    border-radius: 40px;
-    background: linear-gradient(290deg, #419ef1 0%, #9bd0ff 107.89%);
-    color: #fef9f9;
-  }
 `;
 
-export { Header };
+export {
+  Header,
+  NoticePageContrtols,
+  NoticePageContrtolsRight,
+  FilterOpenButton,
+};
