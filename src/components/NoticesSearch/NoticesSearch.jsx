65a7a82ea63e0cad95ef4737322e8a4997574169
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-30 00:39:40 +0200

    notice search style setting

--- src/components/NoticesSearch/NoticesSearch.jsx
+++ src/components/NoticesSearch/NoticesSearch.jsx
@@ -5,7 +5,7 @@ import { SearchForm, SearchInput, SearchButton } from './NoticesSearch.styled';
 const NoticesSearch = () => {
   return (
     <SearchForm>
-      <SearchInput type="text" />
+      <SearchInput type="text" placeholder="search" />
       <SearchButton type="submit">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
           <use xlinkHref={`${icons}#search`} />
--- src/components/NoticesSearch/NoticesSearch.styled.js
+++ src/components/NoticesSearch/NoticesSearch.styled.js
@@ -2,10 +2,32 @@ import styled from '@emotion/styled';
 import { theme } from '../../utils/theme';
 const { fonts, fontSizes, fontWeights, colors } = theme;
 
-const SearchForm = styled.form``;
+const SearchForm = styled.form`
+  width: 608px;
+  height: 44px;
+  display: flex;
+  justify-content: center;
+  border-radius: 20px;
+  background: #fff;
+  padding-left: 20px;
+  padding-right: 10px;
+  box-shadow: 3px 8px 14px 0px rgba(136, 198, 253, 0.19);
+`;
 
-const SearchInput = styled.input``;
+const SearchInput = styled.input`
+  width: 544px;
+  height: 44px;
+  border: none;
+  background: none;
+  outline: none;
+`;
 
-const SearchButton = styled.button``;
+const SearchButton = styled.button`
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
+  border: none;
+  background: none;
+`;
 
 export { SearchForm, SearchInput, SearchButton };
