1c57b719ee86bc2e208898cffba08845f2543d7e
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-30 00:33:23 +0200

    Update NoticesSearch.jsx

--- src/components/NoticesSearch/NoticesSearch.jsx
+++ src/components/NoticesSearch/NoticesSearch.jsx
@@ -1,11 +1,16 @@
 //import { useState } from 'react';
+import icons from '../../images/symbol-defs.svg';
 import { SearchForm, SearchInput, SearchButton } from './NoticesSearch.styled';
 
 const NoticesSearch = () => {
   return (
     <SearchForm>
       <SearchInput type="text" />
-      <SearchButton type="submit"></SearchButton>
+      <SearchButton type="submit">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <use xlinkHref={`${icons}#search`} />
+        </svg>
+      </SearchButton>
     </SearchForm>
   );
 };
