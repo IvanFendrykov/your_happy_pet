768c50829093349eb468f3ff28c09a7e70d7b4f4
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-29 23:55:49 +0200

    Update NoticesPage.styled.js

--- src/pages/NoticesPage/NoticesPage.styled.js
+++ src/pages/NoticesPage/NoticesPage.styled.js
@@ -1,14 +1,14 @@
 import styled from '@emotion/styled';
 import { theme } from '../../utils/theme';
-const { fonts, fontSizes, fontWeights, colors } = theme;
+const { fonts, fontWeights } = theme;
 
 const Header = styled.h3`
   margin-top: 90px;
   color: #111;
-  font-family: 'Manrope', sans-serif;
+  font-family: ${fonts.bold}, sans-serif;
   font-size: 48px;
   font-style: normal;
-  font-weight: 700;
+  font-weight: ${fontWeights.bold};
   text-align: center;
   line-height: normal;
 `;
