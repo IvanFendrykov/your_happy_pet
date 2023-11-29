ff286b1bce737cfdd821267801a3ef032ea0e2d2
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-30 00:28:44 +0200

    Update NoticesSearch.styled.js

--- src/components/NoticesSearch/NoticesSearch.styled.js
+++ src/components/NoticesSearch/NoticesSearch.styled.js
@@ -1,3 +1,11 @@
 import styled from '@emotion/styled';
 import { theme } from '../../utils/theme';
 const { fonts, fontSizes, fontWeights, colors } = theme;
+
+const SearchForm = styled.form``;
+
+const SearchInput = styled.input``;
+
+const SearchButton = styled.button``;
+
+export { SearchForm, SearchInput, SearchButton };
