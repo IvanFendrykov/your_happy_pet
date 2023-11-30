020648ccc1140bafb18fdaa9e1becb3b1be06f45
IvanFendrykov <118548628+IvanFendrykov@users.noreply.github.com>
2023-11-30 07:54:52 +0100

    add dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Suspense } from 'react';
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
@@ -6,11 +5,14 @@ import { Toaster } from 'react-hot-toast';
 import VortexLoader from './components/VortexLoader/VortexLoader';
 import PublicRoute from './PublicRoute/PublicRoute';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
+import { UseTheme } from './hooks/useLocalStorage.js';
+import Layout from './components/Layout/Layout';
+import { ThemeProvider } from './components/ThemeProvader/ThemeProv.jsx';
 
- import Layout from './components/Layout/Layout';
-
-const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage.jsx'));
-import {NoticesCategoriesList} from  './components/NoticesCategoriesList/NoticesCategoriesList';
+const NotFoundPage = lazy(() =>
+  import('./pages/NotFoundPage/NotFoundPage.jsx'),
+);
+import { NoticesCategoriesList } from './components/NoticesCategoriesList/NoticesCategoriesList';
 const AddPetPage = lazy(() => import('./pages/AddPetPage/AddPetPage'));
 const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
 const RegisterPage = lazy(() => import('./pages/RegisterPage/RegisterPage'));
@@ -18,46 +20,48 @@ const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
 const UserPage = lazy(() => import('./pages/UserPage/UserPage'));
 const NewsPage = lazy(() => import('./pages/NewsPage/NewsPage'));
 const NoticesPage = lazy(() => import('./pages/NoticesPage/NoticesPage'));
- const OurFriendsPage = lazy(() =>
-   import('./pages/OurFriendsPage/OurFriendsPage')
- );
+const OurFriendsPage = lazy(() =>
+  import('./pages/OurFriendsPage/OurFriendsPage'),
+);
 
 export const App = () => {
+  const { theme, setTheme } = UseTheme;
   return (
-    <>
-      <Suspense fallback={<VortexLoader />}>
-        <Routes>
-
-          <Route path="/" element={<Layout />}>
-            <Route index element={<MainPage />} />
-            <Route path="main" element={<MainPage />} />
-            <Route path="news" element={<NewsPage />} /> 
-            <Route path="notices" element={<NoticesPage />}>
-              <Route index element={<NoticesCategoriesList />} />
-              <Route path="sell" element={<NoticesCategoriesList />} />
-              <Route path="lost-found" element={<NoticesCategoriesList />} />
-              <Route path="for-free" element={<NoticesCategoriesList />} />
-              <Route path="favorite" element={<NoticesCategoriesList />} />
-              <Route path="own" element={<NoticesCategoriesList />} />
+    <ThemeProvider>
+      <>
+        <Suspense fallback={<VortexLoader />}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<MainPage />} />
+              <Route path="main" element={<MainPage />} />
+              <Route path="news" element={<NewsPage />} />
+              <Route path="notices" element={<NoticesPage />}>
+                <Route index element={<NoticesCategoriesList />} />
+                <Route path="sell" element={<NoticesCategoriesList />} />
+                <Route path="lost-found" element={<NoticesCategoriesList />} />
+                <Route path="for-free" element={<NoticesCategoriesList />} />
+                <Route path="favorite" element={<NoticesCategoriesList />} />
+                <Route path="own" element={<NoticesCategoriesList />} />
+              </Route>
+              <Route path="friends" element={<OurFriendsPage />} />
+              <Route element={<PublicRoute />}>
+                <Route path="register" element={<RegisterPage />} />
+                <Route path="login" element={<LoginPage />} />
+              </Route>
+              <Route element={<PrivateRoute />}>
+                <Route path="user" element={<UserPage />} />
+                <Route path="add-pet" element={<AddPetPage />} />
+              </Route>
+              <Route path="*" element={<NotFoundPage />} />
             </Route>
-            <Route path="friends" element={<OurFriendsPage />} />
-            <Route element={<PublicRoute />}>
-              <Route path="register" element={<RegisterPage />} />
-              <Route path="login" element={<LoginPage />} />
-            </Route>
-            <Route element={<PrivateRoute />}>
-              <Route path="user" element={<UserPage />} />
-              <Route path="add-pet" element={<AddPetPage />} />
-            </Route>
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </Suspense>
-      <Toaster
-        toastOptions={{
-          duration: 3000,
-        }}
-      />
-    </>
+          </Routes>
+        </Suspense>
+        <Toaster
+          toastOptions={{
+            duration: 3000,
+          }}
+        />
+      </>
+    </ThemeProvider>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,24 @@
 import Logo from '../Logo/Logo';
 import Nav from '../Nav/Nav';
 import UserNav from '../UserNav/UserNav';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container, User } from './Header.styled';
 import icon from '../../assets/images/user/user.png';
 import AuthNav from 'components/AuthNav/AuthNav';
 import { useWindowSize } from '../../hooks/useResize';
 import { useAuth } from '../../hooks/useAuth';
 import { Menu } from '../Menu/Menu';
-
+import { ThemeContext } from '../ThemeProvader/ThemeProv';
 const Header = ({ handleClick }) => {
   const size = useWindowSize();
   const { isLoggedIn, user } = useAuth();
-
+const [theme, setTheme] = useContext(ThemeContext);
+const changeTheme = () => {
+  setTheme(theme === 'light' ? 'dark' : 'light')
+}
   return (
     <Container>
+    <button  onClick={changeTheme}>theme</button>
       <Logo size={size} />
       {size[0] >= 768 && size[0] < 1200 && !isLoggedIn && (
         <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
