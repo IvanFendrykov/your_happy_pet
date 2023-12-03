1550c43ecbffe2d9c331504b4f191f8a0fb6b945
OlehKozubJS <oleg.kozub54@gmail.com>
2023-12-03 12:19:07 +0200

    Update AgeForm.jsx

--- src/components/NoticesFilters/AgeForm.jsx
+++ src/components/NoticesFilters/AgeForm.jsx
@@ -27,22 +27,22 @@ const AgeForm = ({ onClick, onChange, initialValue }) => {
       </FilterFormButton>
       <FilterOption
         name="ageOption"
-        value="?gender=up1"
-        checked={ageData === '?gender=up1'}
+        value="gender=up1"
+        checked={ageData === 'gender=up1'}
       >
         up to 1 year
       </FilterOption>
       <FilterOption
         name="ageOption"
-        value="?gender=up2"
-        checked={ageData === '?gender=up2'}
+        value="gender=up2"
+        checked={ageData === 'gender=up2'}
       >
         up to 2 years
       </FilterOption>
       <FilterOption
         name="ageOption"
-        value="?gender=from2"
-        checked={ageData === '?gender=from2'}
+        value="gender=from2"
+        checked={ageData === 'gender=from2'}
       >
         from 2 years
       </FilterOption>
